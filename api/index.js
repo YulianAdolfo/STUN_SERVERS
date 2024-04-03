91dbed188b07c6f004d57ae02908a28377a5e5b2
Yulian <yulian.rojas@noreply.github.com>
2024-04-03 18:52:00 -0500

    API to get STUN/TURN servers with twilio

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require('dotenv').config()
+const app = express();
+
+app.get("/", (req, res) => {
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = require('twilio')(accountSid, authToken);
+
+    let STUN_SERVERS;
+    const servers = async () => await client.tokens.create().then(token => STUN_SERVERS = token.iceServers);
+    servers().then((data)=> res.send(data)).catch((err) => res.send(err))
+});
+
+app.listen(5000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
